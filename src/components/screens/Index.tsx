0447715355921a59fecfa2c1b84bcc8a96f8ceb4
a-Ikaros <873478583@qq.com>
2022-06-14 23:35:36 +0800

    revert:删除firebase

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,15 +1,9 @@
 import { Dialog } from '@headlessui/react';
 import { useRef, useState } from 'react';
 import { useAuthState } from '~/components/contexts/UserContext';
-import { SignInButton } from '~/components/domain/auth/SignInButton';
-import { SignOutButton } from '~/components/domain/auth/SignOutButton';
 import { Head } from '~/components/shared/Head';
 
 function Index() {
-  const { state } = useAuthState();
-  const [isOpen, setIsOpen] = useState(true);
-  const completeButtonRef = useRef(null);
-
   return (
     <>
       <Head title="TOP PAGE" />
@@ -63,35 +57,9 @@ function Index() {
               </a>
               .
             </p>
-            <div className="mt-4 grid gap-2">
-              {state.state === 'UNKNOWN' ? null : state.state === 'SIGNED_OUT' ? <SignInButton /> : <SignOutButton />}
-              <button onClick={() => setIsOpen(true)}>Display Dialog</button>
-            </div>
           </div>
         </div>
       </div>
-      <Dialog
-        className="flex fixed inset-0 z-10 overflow-y-auto"
-        initialFocus={completeButtonRef}
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-      >
-        <div className="flex items-center justify-center min-h-screen w-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-          <div className="relative bg-white rounded max-w-120 p-8 mx-auto">
-            <Dialog.Title>Dialog Title</Dialog.Title>
-            <Dialog.Description>Dialog description</Dialog.Description>
-            <button
-              ref={completeButtonRef}
-              type="button"
-              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-              onClick={() => setIsOpen(false)}
-            >
-              Got it, thanks!
-            </button>
-          </div>
-        </div>
-      </Dialog>
     </>
   );
 }
